fa6662761cc4477c53e5667431056b335d32dfb5
Victor Guschenko <victor.guschenko@gmail.com>
2021-03-06 16:02:31 +0100

    created Class Author

---
+++ dev/src/com/bookmaps/Author.java
@@ -0,0 +1,19 @@
+package com.bookmaps;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
