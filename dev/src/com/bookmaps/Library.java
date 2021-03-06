22c49bea608261843b818b417ac97124924cac9e
Victor Guschenko <victor.guschenko@gmail.com>
2021-03-06 16:03:02 +0100

    created Class Library

---
+++ dev/src/com/bookmaps/Library.java
@@ -0,0 +1,23 @@
+package com.bookmaps;
+
+public class Library {
+    private String address;
+    private boolean isOpen;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+}
